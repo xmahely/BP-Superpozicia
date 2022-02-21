7067356bf241ebf8d058c9865eb4d00643e378a6
xmahely <xmahelyova@stuba.sk>
2022-02-21 17:47:47 +0100

    fix db communication

--- tables/partner.py
+++ tables/partner.py
@@ -18,14 +18,11 @@ class PARTNER(Base):
     Pohlavie = Column(Boolean(), nullable=False)
     Tituly = Column(String(25), nullable=True)
     Datum_Narodenia = Column(Date(), nullable=True)
-    RC = Column(String(10), nullable=True)
     Ulica = Column(String(80), nullable=True)
     Mesto = Column(String(80), nullable=True)
     Kraj = Column(String(80), nullable=True)
     PSC = Column(String(80), nullable=True)
     Danovy_Domicil = Column(String(10), nullable=False)
-    Stlpec2 = Column(String(10), nullable=True)
-    Stlpec3 = Column(String(10), nullable=True)
     Poznamka = Column(String(250), nullable=True)
     Sprac_Dat = Column(DateTime(timezone=True), server_default=func.now())
 
@@ -39,14 +36,11 @@ class PARTNER(Base):
                f"Pohlavie={self.Pohlavie}," \
                f"Tituly={self.Tituly}," \
                f"Datum narodenia={self.Datum_Narodenia}," \
-               f"RC={self.RC}," \
                f"Ulica={self.Ulica}," \
                f"Mesto={self.Mesto}," \
                f"Kraj={self.Kraj}," \
                f"PSC={self.PSC}," \
                f"Danovy domicil={self.Danovy_Domicil}," \
-               f"Stlpec2={self.Stlpec2}," \
-               f"Stlpec3={self.Stlpec3}," \
                f"Poznamka={self.Poznamka}>"
 
     def create(self):
--- tables/partner_norm.py
+++ tables/partner_norm.py
@@ -1,12 +1,17 @@
-from sqlalchemy import Column, BigInteger, Integer, String, Date, text, Boolean, Identity
+
+
+from sqlalchemy import Column, BigInteger, Integer, String, Date, DateTime, Boolean
+from sqlalchemy.sql import func
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.dialects.mssql import BIT
 
 Base = declarative_base()
 
 
-class SUPERPOSITION(Base):
-    __tablename__ = 'Superposition'
+class PARTNER_NORM(Base):
+    __tablename__ = 'Partner_Norm'
     CID = Column(BigInteger(), primary_key=True)
+    Priorita = Column(Integer(), nullable=False)
     Meno = Column(String(50), nullable=False)
     Priezvisko = Column(String(50), nullable=False)
     Pohlavie = Column(Boolean(), nullable=False)
@@ -17,23 +22,23 @@ class SUPERPOSITION(Base):
     Kraj = Column(String(80), nullable=True)
     PSC = Column(String(80), nullable=True)
     Danovy_Domicil = Column(String(10), nullable=False)
-    Identifikatory = Column(String(100), nullable=True)
     Poznamka = Column(String(250), nullable=True)
+    Sprac_Dat = Column(DateTime(timezone=True), server_default=func.now())
 
     def __repr__(self):
         return f"<Klient " \
                f"CID={self.CID}, " \
+               f"Priorita={self.Priorita}," \
                f"Meno={self.Meno}, " \
                f"Priezvisko={self.Priezvisko}.>," \
                f"Pohlavie={self.Pohlavie}," \
                f"Tituly={self.Tituly}," \
                f"Datum narodenia={self.Datum_Narodenia}," \
-               f"RC={self.RC}," \
                f"Ulica={self.Ulica}," \
                f"Mesto={self.Mesto}," \
                f"Kraj={self.Kraj}," \
+               f"PSC={self.PSC}," \
                f"Danovy domicil={self.Danovy_Domicil}," \
-               f"Identifikatory={self.Identifikatory}," \
                f"Poznamka={self.Poznamka}>"
 
     def create(self):
